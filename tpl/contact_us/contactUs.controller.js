a036603d8e9ac423b2f63c2db8214d4934fd0e4d
RoyBarsheshet <roy.barsheshet@gmail.com>
2017-05-01 15:37:51 +0300

    Merge remote-tracking branch 'code_review'

--- tpl/contact_us/contactUs.controller.js
+++ tpl/contact_us/contactUs.controller.js
@@ -1,3 +1,10 @@
 shoeApp.controller("contactUsController", function ($rootScope, $route) {
     $rootScope.title = $route.current.$$route.pageTitle;
+
+
+
+
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').focus()
+})
 })
